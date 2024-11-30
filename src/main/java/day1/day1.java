f0fec3717171a93532fab5219d97116d7d53992e
mg <mathieu.george92@gmail.com>
2024-11-30 19:48:08 +0100

    C'est parti

---
+++ src/main/java/day1/day1.java
@@ -0,0 +1,12 @@
+package day1;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class day1 {
+
+    public static void main(String[] args) throws IOException {
+        Path inputPath = Paths.get("src/main/java/day1/input.txt");
+    }
+}
