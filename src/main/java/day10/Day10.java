8e2d521cfe26c460ee1ae3ee03759f5a6d55d32e
mg <mathieu.george92@gmail.com>
2024-12-11 21:47:16 +0100

    Built MapComponent, works on day10

--- src/main/java/day10/Day10.java
+++ src/main/java/day10/Day10.java
@@ -1,5 +1,7 @@
 package day10;
 
+import map.Coordinate;
+import map.Direction;
 import map.MapComponent;
 
 import java.io.IOException;
@@ -10,36 +12,26 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 
 public class Day10 {
 
-    private static int nbColumns = 0;
-    private static int nbRows = 0;
+    private static MapComponent mc;
 
     public static void main(String[] args) throws IOException {
         Path inputPath = Paths.get("src/main/java/day10/input.txt");
         List<String> lines = Files.readAllLines(inputPath);
 
-        nbColumns = lines.get(0).length();
-        nbRows = lines.size();
-
-        // Init map
-        int[][] map = new int[nbColumns][nbRows];
-        for (int j = 0; j < nbRows; j++) {
-            for (int i = 0; i < nbColumns; i++) {
-                map[j][i] = Character.getNumericValue(lines.get(j).charAt(i));
-            }
-        }
+        mc = new MapComponent(lines.get(0).length(), lines.size());
+        int[][] map = mc.asInts(lines);
 
         // Part 1
-        Map<Coord, Set<Coord>> headsMap = new HashMap<>();
-        for (int j = 0; j < nbRows; j++) {
-            for (int i = 0; i < nbColumns; i++) {
+        Map<Coordinate, Set<Coordinate>> headsMap = new HashMap<>();
+        for (int j = 0; j < mc.nbRows(); j++) {
+            for (int i = 0; i < mc.nbColumns(); i++) {
                 final int nb = map[j][i];
                 if (nb == 0) {
-                    Coord start = new Coord(i, j);
+                    Coordinate start = new Coordinate(i, j);
                     headsMap.put(start, getHeads(map, start, nb, new HashSet<>()));
                 }
             }
@@ -49,12 +41,12 @@ public class Day10 {
         System.out.println(sum); // 698
 
         // Part 2
-        Map<Coord, Integer> pathsMap = new HashMap<>();
-        for (int j = 0; j < nbRows; j++) {
-            for (int i = 0; i < nbColumns; i++) {
+        Map<Coordinate, Integer> pathsMap = new HashMap<>();
+        for (int j = 0; j < mc.nbRows(); j++) {
+            for (int i = 0; i < mc.nbColumns(); i++) {
                 final int nb = map[j][i];
                 if (nb == 0) {
-                    Coord start = new Coord(i, j);
+                    Coordinate start = new Coordinate(i, j);
                     pathsMap.put(start, getPaths(map, start, nb, 0));
                 }
             }
@@ -64,14 +56,14 @@ public class Day10 {
 
     }
 
-    private static int getPaths(int[][] map, Coord start, int currentValue, int sum) {
+    private static int getPaths(int[][] map, Coordinate start, int currentValue, int sum) {
         if (currentValue == 9) {
             return sum + 1;
         }
 
         for (Direction vector : Direction.values()) {
-            Coord next = start.applyDirection(vector);
-            if (isValid(next) && map[next.y][next.x] == currentValue + 1) {
+            Coordinate next = start.apply(vector);
+            if (mc.isValid(next) && map[next.y()][next.x()] == currentValue + 1) {
                 sum = getPaths(map, next, currentValue + 1, sum);
             }
 
@@ -79,53 +71,19 @@ public class Day10 {
         return sum;
     }
 
-    private static Set<Coord> getHeads(int[][] map, Coord start, int currentValue, Set<Coord> set) {
+    private static Set<Coordinate> getHeads(int[][] map, Coordinate start, int currentValue, Set<Coordinate> set) {
         if (currentValue == 9) {
             set.add(start);
             return set;
         }
 
         for (Direction vector : Direction.values()) {
-            Coord next = start.applyDirection(vector);
-            if (isValid(next) && map[next.y][next.x] == currentValue + 1) {
+            Coordinate next = start.apply(vector);
+            if (mc.isValid(next) && map[next.y()][next.x()] == currentValue + 1) {
                 set = getHeads(map, next, currentValue + 1, set);
             }
 
         }
         return set;
-    }
-
-    private static boolean isValid(Coord coord) {
-        return coord.x >= 0 && coord.x < nbColumns && coord.y >= 0 && coord.y < nbRows;
-    }
-
-    enum Direction {
-        UP(0, -1), RIGHT(1, 0), LEFT(-1, 0), DOWN(0, 1);
-
-        final Coord jump;
-
-        Direction(int x, int y) {
-            this.jump = new Coord(x, y);
-        }
-    }
-
-    record Coord(int x, int y) {
-
-        Coord applyDirection(Direction direction) {
-            return new Coord(this.x + direction.jump.x, this.y + direction.jump.y);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Coord coord = (Coord) o;
-            return x == coord.x && y == coord.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
     }
 }
--- src/main/java/map/MapComponent.java
+++ src/main/java/map/MapComponent.java
@@ -1,87 +1,41 @@
 package map;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
-public class MapComponent<T> {
+public record MapComponent(int nbColumns, int nbRows) {
 
-    private final int nbColumns;
-    private final int nbRows;
-    private T[][] innerMap;
-
-    public MapComponent(int nbColumns, int nbRows) {
-        this.nbColumns = nbColumns;
-        this.nbRows = nbRows;
-    }
-
-    public void processInput(List<String> lines) {
-
-    }
-
-    /*private Function<String, T> getMappingFunction(Class<T> clazz) {
-        switch (clazz) {
-            case Integer.class:
-                return Integer::parseInt;
-            case Character.class:
-                return Character::getNumericValue;
-
+    /**
+     * Transforms a list of strings to a 2d-array of integers.
+     * @param lines a given list of strings
+     */
+    public int[][] asInts(List<String> lines) {
+        int[][] map = new int[nbColumns][nbRows];
+        for (int j = 0; j < nbRows; j++) {
+            for (int i = 0; i < nbColumns; i++) {
+                map[j][i] = Character.getNumericValue(lines.get(j).charAt(i));
+            }
         }
+        return map;
+    }
 
-    }*/
-
+    /**
+     * Transforms a list of strings to a 2d-array of chars.
+     * @param lines a given list of strings
+     */
+    public char[][] asChars(List<String> lines) {
+        char[][] map = new char[nbColumns][nbRows];
+        for (int j = 0; j < nbRows; j++) {
+            for (int i = 0; i < nbColumns; i++) {
+                map[j][i] = lines.get(j).charAt(i);
+            }
+        }
+        return map;
+    }
 
     /**
      * Returns true if a coordinate is within bounds.
      */
     public boolean isValid(Coordinate coordinate) {
-        return coordinate.x >= 0 && coordinate.x < nbColumns && coordinate.y >= 0 && coordinate.y < nbRows;
-    }
-
-    /**
-     * Directions in a map with their given vector, no diagonals.
-     */
-    public enum Direction {
-        UP(0, -1), RIGHT(1, 0), LEFT(-1, 0), DOWN(0, 1);
-
-        final Coordinate vector;
-
-        Direction(int x, int y) {
-            this.vector = new Coordinate(x, y);
-        }
-    }
-
-    /**
-     * Record to use coordinates in a 2d-array with x and y coordinates.
-     * @param x the x coordinate (start at 0), horizontal axis
-     * @param y the y coordinate (starts at 0), vertical axis
-     */
-    public record Coordinate(int x, int y) {
-
-        Coordinate apply(Direction direction) {
-            return new Coordinate(this.x + direction.vector.x, this.y + direction.vector.y);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Coordinate coordinate = (Coordinate) o;
-            return x == coordinate.x && y == coordinate.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
-
-    public int getNbColumns() {
-        return nbColumns;
-    }
-
-    public int getNbRows() {
-        return nbRows;
+        return coordinate.x() >= 0 && coordinate.x() < nbColumns && coordinate.y() >= 0 && coordinate.y() < nbRows;
     }
 }
